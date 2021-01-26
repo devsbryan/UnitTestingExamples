da4d85a78b70dc643f69084661015e6f6fc195e6
Simon Bryan <simon.bryan@certara.com>
2021-01-26 13:01:10 +0000

    Add simple product controller that calls a product service to return a product model

---
+++ src/MyCompany.ECommerce.Api/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyCompany.ECommerce.Api.Services;
+
+namespace MyCompany.ECommerce.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public IActionResult GetProduct(int id)
+        {
+            var productService = new ProductService();
+
+            var product = productService.GetProduct(id);
+
+            return new OkObjectResult(product);
+        }
+    }
+}
---
+++ src/MyCompany.ECommerce.Api/Services/ProductService.cs
@@ -0,0 +1,12 @@
+﻿using MyCompany.ECommerce.Api.Models;
+
+namespace MyCompany.ECommerce.Api.Services
+{
+    public class ProductService
+    {
+        public Product GetProduct(int id)
+        {
+            return new Product();
+        }
+    }
+}
