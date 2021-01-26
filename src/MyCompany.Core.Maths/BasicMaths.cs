0981febbeaae548228b1bd84abbec3c53af7fd95
Simon Bryan <simon.bryan@certara.com>
2021-01-26 12:29:43 +0000

    Add BasicMaths class

---
+++ src/MyCompany.Core.Maths/BasicMaths.cs
@@ -0,0 +1,25 @@
+﻿namespace MyCompany.Core.Maths
+{
+    public class BasicMaths
+    {
+        public double Add(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+
+        public double Subtract(double number1, double number2)
+        {
+            return number1 - number2;
+        }
+
+        public double Divide(double number1, double number2)
+        {
+            return number1 / number2;
+        }
+
+        public double Multiply(double number1, double number2)
+        {
+            return number1 * number2;
+        }
+    }
+}
